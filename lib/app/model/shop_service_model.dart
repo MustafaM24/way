6465389373a288618f20e1cdb2a86a4ef1181478
Mustafa Madraswala <87267526+MustafaM24@users.noreply.github.com>
2023-04-24 21:28:46 +0500

    Delete lib directory

--- lib/app/model/shop_service_model.dart
+++
@@ -1,7 +0,0 @@
-class ShopServiceModel {
-  final String serviceName;
-  final String serialNo;
-  final String time;
-  ShopServiceModel(
-      {required this.serviceName, required this.serialNo, required this.time});
-}
