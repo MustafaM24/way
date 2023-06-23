3775df03838255aff147e1aef3eb4ae091e6859d
Mustafa Madraswala <87267526+MustafaM24@users.noreply.github.com>
2023-06-23 16:54:48 +0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:way/router/router.dart' as router;
 import 'package:flutter/services.dart';
 import 'package:way/router/router.dart';
 import 'package:way/theme/theme.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
